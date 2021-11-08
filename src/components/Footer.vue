9c0375c8c2574ca27bbaecb5fe137a339571b1ef
yujin <sally@moducompany.com>
2021-11-08 16:11:14 +0900

    vue 기초!

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <p>footer</p>
+    <router-link to="/about">About</router-link>
+  </footer>
+</template>
+
+<style>
+a {
+  color: #333;
+}
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
